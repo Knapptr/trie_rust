e2285a755adad5407a351c92e2a6941f56a87f66
knapptr <knapptr@gmail.com>
2023-03-25 15:41:41 -0400

    initial hashmap version

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::{path::Path, thread::current};
+use std::{collections::HashMap, path::Path};
 
 #[derive(Clone, Debug)]
 struct NodeList {
-    nodes: Vec<Option<Box<Node>>>,
+    // nodes: Vec<Option<Box<Node>>>,
+    nodes: HashMap<char, Node>,
 }
 #[derive(Clone, Debug)]
 struct Node {
@@ -13,19 +14,20 @@ struct Node {
 impl NodeList {
     fn new() -> Self {
         NodeList {
-            nodes: vec![None; 26],
+            nodes: HashMap::new(),
         }
     }
     fn add_char(&mut self, char: char, terminal: bool) {
-        let char_value = (char as u8 - b'a') as usize;
-        match &mut self.nodes[char_value] {
+        match &mut self.nodes.get_mut(&char) {
             Some(child) => {
                 if child.terminal {
                     return;
                 }
                 child.terminal = terminal;
             }
-            None => self.nodes[char_value] = Some(Box::new(Node::new(char, terminal))),
+            None => {
+                self.nodes.insert(char, Node::new(char, terminal));
+            }
         }
     }
     fn add_word(&mut self, word: &str) {
@@ -37,9 +39,8 @@ impl NodeList {
                 current_list.add_char(char, true)
             } else {
                 current_list.add_char(char, false);
-                let letter_index = (char as u8 - b'a') as usize;
 
-                match current_list.nodes[letter_index].as_mut() {
+                match current_list.nodes.get_mut(&char) {
                     Some(list) => current_list = &mut list.children,
                     None => unreachable!(),
                 }
@@ -56,13 +57,8 @@ impl NodeList {
         }
         words
     }
-    fn existing_nodes(&self) -> Option<Vec<&Box<Node>>> {
-        let children: Vec<&Box<Node>> = self
-            .nodes
-            .iter()
-            .filter(|c| c.is_some())
-            .map(|n| n.as_ref().unwrap())
-            .collect();
+    fn existing_nodes(&self) -> Option<Vec<&Node>> {
+        let children: Vec<&Node> = self.nodes.iter().map(|(_, node)| node).collect();
         if children.len() == 0 {
             return None;
         }
@@ -71,8 +67,7 @@ impl NodeList {
     fn find_starts_with(&self, str: &str) -> Vec<String> {
         let mut current_index = self;
         for char in str.chars() {
-            let value = (char as u8 - b'a') as usize;
-            if let Some(current_node) = &current_index.nodes[value] {
+            if let Some(current_node) = current_index.nodes.get(&char) {
                 current_index = &current_node.children;
             } else {
                 panic!()
@@ -93,7 +88,7 @@ impl Node {
             terminal,
         }
     }
-    fn get_children(&self) -> Option<Vec<&Box<Node>>> {
+    fn get_children(&self) -> Option<Vec<&Node>> {
         self.children.existing_nodes()
     }
     fn words_from(&self) -> Vec<String> {
@@ -132,21 +127,18 @@ fn main() {
     // println!("{:?}", root.get_all_words());
     let words = std::fs::read_to_string(Path::new("/usr/share/dict/american-english")).unwrap();
     for word in words.split("\n") {
-        if word.contains(|c: char| !c.is_ascii_alphabetic()) {
-            continue;
-        }
         root.add_word(&word.to_ascii_lowercase());
     }
-    println!("{:?} Words Added", root.get_all_words().len());
-    println!("Search for:");
-    let mut usr_input = String::new();
-    std::io::stdin().read_line(&mut usr_input).unwrap();
-    usr_input = usr_input.trim().to_string();
-    println!(
-        "Begin with {}: {:?}",
-        &usr_input,
-        root.find_starts_with(&usr_input)
-    );
+    // println!("{:?} Words Added", root.get_all_words().len());
+    // println!("Search for:");
+    // let mut usr_input = String::new();
+    // std::io::stdin().read_line(&mut usr_input).unwrap();
+    // usr_input = usr_input.trim().to_string();
+    // println!(
+    //     "Begin with {}: {:?}",
+    //     &usr_input,
+    //     root.find_starts_with(&usr_input)
+    // );
 }
 
 #[cfg(test)]
